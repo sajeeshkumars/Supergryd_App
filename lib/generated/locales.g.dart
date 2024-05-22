f5c0248627debe32694e010561106335f3c92ca7
Najib-seeroo <najib.v@seeroo.com>
2024-05-22 19:30:41 +0530

    home apis

---
+++ lib/generated/locales.g.dart
@@ -0,0 +1,48 @@
+// DO NOT EDIT. This is code generated via package:get_cli/get_cli.dart
+
+// ignore_for_file: lines_longer_than_80_chars
+// ignore: avoid_classes_with_only_static_members
+class AppTranslation {
+  static Map<String, Map<String, String>> translations = {
+    'en_US': Locales.en_US,
+  };
+}
+
+class LocaleKeys {
+  LocaleKeys._();
+  static const your_location = 'your_location';
+  static const hi = 'hi';
+  static const explore_the_popular_category = 'explore_the_popular_category';
+  static const food = 'food';
+  static const cabs = 'cabs';
+  static const Salon_services = 'Salon_services';
+  static const home_services = 'home_services';
+  static const gifts = 'gifts';
+  static const furniture = 'furniture';
+  static const courier = 'courier';
+  static const grocery = 'grocery';
+  static const others = 'others';
+  static const select_your_delivery_location = 'select_your_delivery_location';
+  static const add_new_address = 'add_new_address';
+  static const saved_address = 'saved_address';
+}
+
+class Locales {
+  static const en_US = {
+    'your_location': 'Your Location',
+    'hi': 'Hi',
+    'explore_the_popular_category': 'Explore The Popular Categories',
+    'food': 'Food',
+    'cabs': 'Cabs',
+    'Salon_services': 'Salon Services',
+    'home_services': 'Home Services',
+    'gifts': 'Gifts',
+    'furniture': 'Furniture',
+    'courier': 'Courier',
+    'grocery': 'Grocery',
+    'others': 'Others',
+    'select_your_delivery_location': 'Select Your Delivery Location',
+    'add_new_address': 'Add New Address',
+    'saved_address': 'Saved Address',
+  };
+}
