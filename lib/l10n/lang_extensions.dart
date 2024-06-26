2fa4c42921c8234976e1bd1ce71d6d20ce1941de
AdarshKSeeroo <144668088+AdarshKSeeroo@users.noreply.github.com>
2024-06-26 10:32:27 +0530

    added trycatch

--- lib/l10n/lang_extensions.dart
+++ lib/l10n/lang_extensions.dart
@@ -1,7 +1,16 @@
+import 'dart:developer';
+
 import 'package:flutter/cupertino.dart';
 
 import 'app_localizations.dart';
 
 extension LocalizedBuildContext on BuildContext {
-  AppLocalizations? get loc => AppLocalizations.of(this);
+  AppLocalizations? get loc {
+    try {
+      return AppLocalizations.of(this);
+    } catch (e, stack) {
+      log("Exception occurred $e,stack$stack", name: "Localized");
+      return null;
+    }
+  }
 }
